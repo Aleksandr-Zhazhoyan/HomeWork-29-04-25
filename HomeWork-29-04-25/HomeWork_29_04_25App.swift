c9a504b443db8ef1bbf2f3152521dbdad78dfc46
Aleksandr-Zhazhoyan <zhazhoyan85@gmail.com>
2025-05-06 15:29:51 +0400

    Initial Commit

---
+++ HomeWork-29-04-25/HomeWork_29_04_25App.swift
@@ -0,0 +1,17 @@
+//
+//  HomeWork_29_04_25App.swift
+//  HomeWork-29-04-25
+//
+//  Created by Aleksandr Zhazhoyan on 06.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct HomeWork_29_04_25App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
